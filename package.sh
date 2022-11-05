99e1c2b36e95ae4834ebc0aa15e5866936bc9822
zdcthomas <zdcthomas@yahoo.com>
2022-11-05 15:54:36 -0600

    adds packaging script for zipping into manual loadable format

---
+++ package.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+pnpm build
+
+rm -rf build
+mkdir -p build/"$1"/dist
+cd build || exit
+
+cp ../dist/index.js "$1"/dist/index.js
+cp ../*.py "$1"
+cp ../package.json "$1"
+cp ../plugin.json "$1"
+cp ../LICENSE "$1"
+cp ../README.md "$1"
+
+zip "$1-$2.zip" "$1"/dist/index.js
+zip "$1-$2.zip" "$1/"*.py
+zip "$1-$2.zip" "$1/"*.json
+zip "$1-$2.zip" "$1"/LICENSE
+zip "$1-$2.zip" "$1"/README.md
