bee83c8462cb4ff2676ad0cdd39f270cdcc9cac0
zdcthomas <zdcthomas@yahoo.com>
2022-11-05 17:11:37 -0600

    update release.sh to scp build to deck

--- release.sh
+++ release.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
+PACKAGE_NAME="Dict"
+STEAMDECK_IP="192.168.1.11"
+
 pnpm build
 
 rm -rf build
-mkdir -p build/"$1"/dist
+mkdir -p build/"$PACKAGE_NAME"/dist
 cd build || exit
 
-cp ../dist/index.js "$1"/dist/index.js
-cp ../*.py "$1"
-cp ../package.json "$1"
-cp ../plugin.json "$1"
-cp ../LICENSE "$1"
-cp ../README.md "$1"
+cp ../dist/index.js "$PACKAGE_NAME"/dist/index.js
+cp ../*.py "$PACKAGE_NAME"
+cp ../package.json "$PACKAGE_NAME"
+cp ../plugin.json "$PACKAGE_NAME"
+cp ../LICENSE "$PACKAGE_NAME"
+cp ../README.md "$PACKAGE_NAME"
 
-ZIP_NAME="$1-$2.zip"
+ZIP_NAME="$PACKAGE_NAME-$1.zip"
 
-zip $ZIP_NAME "$1"/dist/index.js
-zip $ZIP_NAME "$1/"*.py
-zip $ZIP_NAME "$1/"*.json
-zip $ZIP_NAME "$1"/LICENSE
-zip $ZIP_NAME "$1"/README.md
+zip $ZIP_NAME "$PACKAGE_NAME"/dist/index.js
+zip $ZIP_NAME "$PACKAGE_NAME/"*.py
+zip $ZIP_NAME "$PACKAGE_NAME/"*.json
+zip $ZIP_NAME "$PACKAGE_NAME"/LICENSE
+zip $ZIP_NAME "$PACKAGE_NAME"/README.md
 
-gh release create $2 $ZIP_NAME
+# gh release create $1 $ZIP_NAME
 # https://github.com/zdcthomas/decky_dict/archive/refs/tags/0.0.1.zip
+# https://github.com/zdcthomas/decky_dict/releases/download/v0.0.1/Dict-v0.0.1.zip
+
+scp -r $PACKAGE_NAME deck@"$STEAMDECK_IP":homebrew/plugins
+ssh -t deck@"$STEAMDECK_IP" "sudo rm -rf ~/homebrew/plugins/$PACKAGE_NAME; sudo mv ~/homebrew/$PACKAGE_NAME ~/homebrew/plugins/"
